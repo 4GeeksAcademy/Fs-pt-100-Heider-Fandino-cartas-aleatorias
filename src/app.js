4462246d815a7375473bb8c836267885a1baf8bd
Heider.F <heiderfandino@gmail.com>
2025-04-11 22:46:19 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = function () {
+  let tipoCarta = ["Diamantes", "Espada", "Corazon", "Trebols"];
+  let Cartas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  let NumeroAleatorio = Math.floor(Math.random() * Cartas.length);
+  let tipoCartaAleatorio = Math.floor(Math.random() * tipoCarta.length);
+  let cartaFinal = tipoCarta[tipoCartaAleatorio];
+
+  // Mostrar el número/letra de la carta
+  document.getElementById("contenedorcarta").innerHTML = Cartas[NumeroAleatorio];
+
+  // Añadir clases para estilo
+  document.getElementById("carta").className = "";
+  document.getElementById("carta").classList.add("card");
+  document.getElementById("carta").classList.add(ObteniendoCarta(cartaFinal));
+
   console.log("Hello Rigo from the console!");
 };
+
+// Función corregida
+function ObteniendoCarta(tipoDeCarta) {
+  switch (tipoDeCarta) {
+    case "Diamantes":
+      return "tipoDeCarta-diamantes";
+    case "Espada":
+      return "tipoDeCarta-espada";
+    case "Corazon":
+      return "tipoDeCarta-corazon";
+    case "Trebols":
+      return "tipoDeCarta-trebols";
+    default:
+      return "";
+  }
+}
